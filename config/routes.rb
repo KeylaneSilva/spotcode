c369833ca64f1f6d8224aff0dfb694386d639af3
Keylane Silva <keylanessilva07@gmail.com>
2020-04-13 15:37:10 -0300

    /

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
   root "home#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: { format: :json} do
+    namespace :v1 do
+      resources :dashboard, only: :index
+    end
+  end
 end
