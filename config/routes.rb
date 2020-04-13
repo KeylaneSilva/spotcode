5305aff1f8ebaffa6fab1284f9dad624f9cb0e63
Keylane Silva <keylanessilva07@gmail.com>
2020-04-13 16:26:40 -0300

    /

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json} do
     namespace :v1 do
       resources :dashboard, only: :index
+      resources :categories, only: :index
+      resources :search, only: :index
     end
   end
 end
