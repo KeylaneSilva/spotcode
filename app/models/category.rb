4aeb83dc787ef91dda504a7b565df1240fad6c69
Keylane Silva <keylanessilva07@gmail.com>
2020-04-14 18:41:46 -0300

    /

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,15 @@
 class Category < ApplicationRecord
     has_many :albums
-
     validates :name, presence: true
     has_one_attached :image
+
+    def artists
+        Artists.joins(:albums).where(albums: { id: self.albums.ids}).distinct
+    end
+
+    def songs
+        Song.joins(:albums).where(songs: { id: self.albums.ids}).distinct
+    end
+
+
 end
